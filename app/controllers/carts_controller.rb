3101a47fe7fbb31bf5bf8b4c5a4e9675399349b1
MACorman <madelinecorman@gmail.com>
2020-02-12 13:11:00 -0500

    added create actions to cart and product-carts controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -4,4 +4,14 @@ class CartsController < ApplicationController
         carts = Cart.all 
         render json: carts
     end
+
+    def create
+        cart = Cart.create(cart_params)
+        render json: cart
+    end
+
+    private
+    def cart_params
+        params.require(:cart).permit(:user_id)
+    end
 end
--- app/controllers/product_carts_controller.rb
+++ app/controllers/product_carts_controller.rb
@@ -4,4 +4,14 @@ class ProductCartsController < ApplicationController
         product_carts = ProductCart.all 
         render json: product_carts
     end
+
+    def create
+        product_cart = ProductCart.create(product_carts_params)
+        render json: product_cart
+    end
+
+    private
+    def product_carts_params
+        params.require(:product_cart).permit(:product_id, :cart_id)
+    end
 end
