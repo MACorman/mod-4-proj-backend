ae2d085c34a6f0dd95c5cd1567990651fa396b73
Rines.io <tsrines@gmail.com>
2020-02-13 09:41:32 -0500

    message

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -5,4 +5,14 @@ class InventoriesController < ApplicationController
         render json: inventories
     end
 
+    def create
+        inventory = Inventory.find_or_create_by(inventory_params)
+        render json: inventory
+    end
+
+    private
+    def inventory_params
+        params.require(:inventory).permit(:user_id)
+    end
+
 end
